39d2c6af52c1cf2cda065a20af5792b912b5f0f9
Tobiasz <tobi605@gmail.com>
2018-04-18 12:28:00 +0200

    Initial commit

---
+++ app/src/main/java/com/polibuda/gimbus/android_lab7/GPSActivity.java
@@ -0,0 +1,75 @@
+package com.polibuda.gimbus.android_lab7;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GPSActivity extends AppCompatActivity implements LocationListener {
+    private LocationManager manager;
+    private TextView locationVal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps);
+        locationVal = findViewById(R.id.location);
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
+        }
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Altitude: ");
+        builder.append(location.getAltitude());
+        builder.append("\n");
+        builder.append("Longitude: ");
+        builder.append(location.getLongitude());
+        builder.append("\n");
+        builder.append("Latitude: ");
+        builder.append(location.getLatitude());
+        builder.append("\n");
+        locationVal.setText(builder.toString());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.removeUpdates(this);
+    }
+}
---
+++ app/src/main/java/com/polibuda/gimbus/android_lab7/MainActivity.java
@@ -0,0 +1,80 @@
+package com.polibuda.gimbus.android_lab7;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView accel;
+    private TextView light;
+    private SensorManager manager;
+    private Sensor lightSensor;
+    private Sensor accelSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        accel = findViewById(R.id.accel_val);
+        light = findViewById(R.id.light_val);
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        accelSensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GPSActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()){
+            case Sensor.TYPE_ACCELEROMETER:{
+                StringBuilder builder = new StringBuilder();
+                builder.append("x: ");
+                builder.append(String.format("%7.4f",event.values[0]));
+                builder.append(" y: ");
+                builder.append(String.format("%7.4f",event.values[1]));
+                builder.append(" z: ");
+                builder.append(String.format("%7.4f",event.values[2]));
+                accel.setText(builder.toString());
+            }
+            case Sensor.TYPE_LIGHT:{
+                StringBuilder builder = new StringBuilder();
+                builder.append(String.format("%7.4f",event.values[0]));
+                builder.append(" lumen");
+                light.setText(builder.toString());
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        manager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        manager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterListener(this);
+    }
+}
